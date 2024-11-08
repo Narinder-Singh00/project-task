04891e41b98194068f6e5003b00a144088f16436
Narinder-Singh00 <narinderkhuttan1005@gmail.com>
2024-11-08 18:29:42 +0530

    uploaded

--- task5.js
+++ task5.js
@@ -1,3 +1,4 @@
-function color(){
-    
+function BackgroundColor() {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
 }
--- task7.js
+++ task7.js
@@ -1,3 +1,6 @@
-function submit(){
-    
+function handleSubmit(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    alert(`Name: ${name}, Age: ${age}`);
 }
